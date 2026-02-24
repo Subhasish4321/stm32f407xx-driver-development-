869b790e7db2a5b1511808e39999d79dfc1968f5
Subhasish Singha <125857603+Subhasish4321@users.noreply.github.com>
2026-02-24 11:54:21 +0530

    Update stm32f407xx.h

--- stm32f4xx_drivers/drivers/inc/stm32f407xx.h
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx.h
@@ -215,6 +215,19 @@ typedef struct
  */
 #define SYSCFG_CLK_DIS()    (RCC->APB2ENR &= ~(1 << 14))
 /*
+ * Macros to reset GPIOx peripherals.
+ * Note: we will first set the bit and then do a reset of the same bit for the purpose explained in DeInit() function
+ */
+#define GPIOA_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 0)); (RCC->AHB1RSTR &= ~(1 << 0)); } while(0)
+#define GPIOB_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 1)); (RCC->AHB1RSTR &= ~(1 << 1)); } while(0)
+#define GPIOC_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 2)); (RCC->AHB1RSTR &= ~(1 << 2)); } while(0)
+#define GPIOD_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 3)); (RCC->AHB1RSTR &= ~(1 << 3)); } while(0)
+#define GPIOE_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 4)); (RCC->AHB1RSTR &= ~(1 << 4)); } while(0)
+#define GPIOF_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 5)); (RCC->AHB1RSTR &= ~(1 << 5)); } while(0)
+#define GPIOG_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 6)); (RCC->AHB1RSTR &= ~(1 << 6)); } while(0)
+#define GPIOH_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 7)); (RCC->AHB1RSTR &= ~(1 << 7)); } while(0)
+#define GPIOI_REG_RESET()     do{ (RCC->AHB1RSTR |= (1 << 8)); (RCC->AHB1RSTR &= ~(1 << 8)); } while(0)
+/*
  * Some generic Macros
  */
 #define ENABLE    1
@@ -225,3 +238,4 @@ typedef struct
 #define GPIO_PIN_RESET   0
 
 #endif /* INC_STM32F407XX_H_ */
+
