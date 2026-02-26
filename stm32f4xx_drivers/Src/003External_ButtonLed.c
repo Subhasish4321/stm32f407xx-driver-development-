acd1726fb1433ca9b80b75b6d7586825ad5b84c6
Subhasish Singha <125857603+Subhasish4321@users.noreply.github.com>
2026-02-26 15:46:13 +0530

    Create 003External_ButtonLed.c

---
+++ stm32f4xx_drivers/Src/003External_ButtonLed.c
@@ -0,0 +1,12 @@
+/*
+* Write a program to connect external button to the pin number PB12 and external LED to PA14
+* and toggle the LED when the extenal button in pressed.
+*/
+
+int main(void)
+{
+// peripheral Clock starts
+  
+//GPIO peripheral init configurations
+
+}
