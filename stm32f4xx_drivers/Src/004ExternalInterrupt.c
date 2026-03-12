97bf9edeeea056ba7d6cbe7ac316918f4380b7cb
Subhasish Singha <125857603+Subhasish4321@users.noreply.github.com>
2026-03-12 08:24:39 +0530

    Update 004ExternalInterrupt.c

--- stm32f4xx_drivers/Src/004ExternalInterrupt.c
+++ stm32f4xx_drivers/Src/004ExternalInterrupt.c
@@ -13,11 +13,49 @@
 * 6. Enable Interrupt reception on that IRQ number(processor side).
 * 7.Implement the IRQ handler.
 */
+#include "stm32f407xx.h"
+#include <string.h>
+
+// Software delay
+void soft_delay()
+{
+	//this will introduce ~200ms delay when system cloc is 16mhz
+	for(uint32_t i = 0;i <500000/2;i++);
+}
+
 int main(void)
 {
+	GPIO_Handle_t GPIOBtn, GPIOLed;
+	memset(&GPIOBtn , 0 ,sizeof(GPIOBtn)); // need to do this to prevent register corruption bugs.
+	memset(&GPIOLed , 0 ,sizeof(GPIOLed));
+	//BUTTON
+	GPIOBtn.pGPIOx = GPIOD;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_FT;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
+    //LED
+	GPIOLed.pGPIOx = GPIOD;
+	GPIOLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIOLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GPIOLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	GPIOLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	// peripheral Clock starts
+	GPIO_PeriClockControl(GPIOD,ENABLE);
+	GPIO_Init(&GPIOLed);
+	GPIO_Init(&GPIOBtn);
 
+	//IRQ configuration
+	GPIO_IRQPriorityConfig(IRQ_NO_EXTI9_5,NVIC_IRQ_PRI15 );
+	GPIO_IRQ_IT_Config(IRQ_NO_EXTI9_5 , ENABLE);
+
+	while(1);
 }
-void EXTI0_IRQHandler(void)// defined the already existing ISR in the startup file.
+void EXTI9_5_IRQHandler(void)// defined the already existing ISR in the startup file.
 {
-	GPIO_IRQHandling();//calling user defined IRQ Handler.
+	soft_delay();//prevents multiple interrupt debounce error in one press.
+	GPIO_IRQHandling(GPIO_PIN_NO_5);//calling user defined IRQ Handler. It just resets the pending register.
+	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 }
