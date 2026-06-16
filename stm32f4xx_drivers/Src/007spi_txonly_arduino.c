8da5f92c512de5fffb0317cd508e871045a9c699
Subhasish Singha <125857603+Subhasish4321@users.noreply.github.com>
2026-06-16 10:09:19 +0530

    Update 007spi_txonly_arduino.c

--- stm32f4xx_drivers/Src/007spi_txonly_arduino.c
+++ stm32f4xx_drivers/Src/007spi_txonly_arduino.c
@@ -1,41 +1,71 @@
-#include "stm32f407xx.h"
-#include <string.h>
-
-void delay()
-{
-	for(uint32_t i = 0;i < 999999;i++);
-}
-/**
- * SPI Pins
- * SPI2_MISO PB14 in AF5 mode
- * SPI2_MOSI PB15 in AF5 mode
- * SPI2_NSS PB12  in AF5
- * SPI2_SCK PB13 in AF5
- * @details This Function is used to initialze the GPIO pins to behave as SPI Pins.
- * @note - The SPI_GPIOInit is not a driver API instead it is an application level function,that uses the driver API GPIOInit()
+/*
+ * 006spi_tx_testing.c
+ *
+ *  Created on: Feb 10, 2019
+ *      Author: admin
  */
-void SPI2_GPIOInit(void)
+
+#include<string.h>
+#include "stm32f407xx.h"
+#include <stdio.h>
+
+extern void initialise_monitor_handles();
+void delay(void)
 {
-    GPIO_Handle_t SpiPins;
-    SpiPins.pGPIOx = GPIOB;
-    SpiPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
-    SpiPins.GPIO_PinConfig.GPIO_PinAltFunMode = GPIO_ALT_FUN_MODE_5;
-    SpiPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
-    SpiPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
-    SpiPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
-
-    //Enable the peripheral clock
-	GPIO_PeriClockControl(GPIOB,ENABLE);
-    //MOSI
-    SpiPins.GPIO_PinConfig.GPIO_PinNumber = 15;
-    GPIO_Init(&SpiPins);
-
-     SpiPins.GPIO_PinConfig.GPIO_PinNumber = 13;
-    GPIO_Init(&SpiPins);
-    //NSS
-    SpiPins.GPIO_PinConfig.GPIO_PinNumber = 12;
-    GPIO_Init(&SpiPins);
+	for(uint32_t i = 0 ; i < 500000 ; i ++);
 }
+
+/*
+ * PB14 --> SPI2_MISO
+ * PB15 --> SPI2_MOSI
+ * PB13 -> SPI2_SCLK
+ * PB12 --> SPI2_NSS
+ * ALT function mode : 5
+ */
+
+/* SPI Slave Demo
+
+ *
+ * SPI pin numbers:
+ * SCK   13  // Serial Clock.
+ * MISO  12  // Master In Slave Out.
+ * MOSI  11  // Master Out Slave In.
+ * SS    10  // Slave Select . Arduino SPI pins respond only if SS pulled low by the master
+ *
+
+ */
+
+void SPI2_GPIOInits(void)
+{
+	GPIO_Handle_t SPIPins;
+
+	SPIPins.pGPIOx = GPIOB;
+	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIO_PeriClockControl(GPIOB, ENABLE);
+	//SCLK
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+
+	//MOSI
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&SPIPins);
+
+	//MISO
+	//SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+	//GPIO_Init(&SPIPins);
+
+
+	//NSS
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GPIO_Init(&SPIPins);
+
+
+}
+
 void SPI2_Init(void)
 {
     SPI_Handle_t SPIHandle;
@@ -45,7 +75,7 @@ void SPI2_Init(void)
     SPIHandle.SPI_Config.SPI_CPHA = SPI_CPHA_LOW;
     SPIHandle.SPI_Config.SPI_CPOL = SPI_CPOL_LOW;
     SPIHandle.SPI_Config.SPI_DFF = SPI_DFF_8BITS;
-    SPIHandle.SPI_Config.SPI_SclkSpeed = SPI_SCK_SPEED_DIV8;//for 2 MHz clock we divide by 8.
+    SPIHandle.SPI_Config.SPI_SclkSpeed = SPI_SCK_SPEED_DIV256;
     SPIHandle.SPI_Config.SPI_SSM = SPI_SSM_DISABLE;//Hardware Slave management.
     SPI_PeriClockControl(SPI2,ENABLE);
     SPI_Init(&SPIHandle);
@@ -60,52 +90,76 @@ void SPI2_GPIOButtonInit(void)
 	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
+
+//	GPIO_Handle_t GPIOPin;
+//	GPIOPin.pGPIOx = GPIOA;
+//	GPIOPin.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+//	GPIOPin.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+//	GPIOPin.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+//	GPIOPin.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+//	GPIOPin.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
 	GPIO_PeriClockControl(GPIOA, ENABLE); //Enable peripheral clock.
 
 	GPIO_Init(&GPIOBtn);
+//	GPIO_Init(&GPIOPin);
+
 }
-/**
- * @Note In this case we have a slave so we use SSM = 0. On the slave side the SS pin should be pulled to zero for it
- * to be active. So we want control the SS of slave according to SPE of master. i.e. when SSM =0.SPE= 0 then SS = 1
- * and when SSM =0, SPE =1 then SS should be 0. This can be achieved using the SSOE(Slave Select output enabled bit)
- */
-int main()
+
+
+int main(void)
 {
-    char data[] = "Hello World";
+	char user_data[] = "hello";
+	initialise_monitor_handles();
+	SPI2_GPIOButtonInit();
+//    printf("Printing is on \n");
+	//this function is used to initialize the GPIO pins to behave as SPI2 pins
+	SPI2_GPIOInits();
 
-    /*Initialize GPIO for SPI Configuration Pins*/
-    SPI2_GPIOInit();
+//	while(1)
+//	{
+//		uint8_t button_state = GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0);
+//		delay();
+//		printf("Button state: %d \n",button_state);
+//	}
+	//This function is used to initialize the SPI2 peripheral parameters
+	SPI2_Init();
 
-    /*Initialize GPIO Button for sending the data on button press */
-    SPI2_GPIOButtonInit();
+	/*
+	* making SSOE 1 does NSS output enable.
+	* The NSS pin is automatically managed by the hardware.
+	* i.e when SPE=1 , NSS will be pulled to low
+	* and NSS pin will be high when SPE=0
+	*/
+	SPI_SSOE_Config(SPI2,ENABLE);
+	while(1)
+	{
+		//wait till button is pressed
+		while( ! GPIO_ReadFromInputPin(GPIOA,GPIO_PIN_NO_0) );
 
-    /*Initialize SPI*/
-    SPI2_Init();
-    while(1)
-    {
-    	/*Send Data only after button pressed*/
-		while(!GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_0));
-		/*Delay to avoid debouncing */
+		//to avoid button de-bouncing related issues 200ms of delay
 		delay();
 
-		/*Slave Select control for single master using SSOE,see note for more info*/
-		SPI_SSOE_Config(SPI2,ENABLE);
-
-		/*Enable SPI*/
+		//enable the SPI2 peripheral
 		SPI_EnableOrDisable(SPI2,ENABLE);
+//		GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_5, 0);//using alternate pin since NSS seems to be not working.
+		delay();
+//		uint8_t Nss_state = GPIO_ReadFromInputPin(GPIOA, GPIO_PIN_NO_5);
+//        printf("The pa5 is %d \n",Nss_state);
+		//first send length information
+		uint8_t dataLen = strlen(user_data);
+		SPI_SendData(SPI2,&dataLen,1);
+//		delay();
+		//to send data
+		SPI_SendData(SPI2,(uint8_t*)user_data,strlen(user_data));
 
-		/*Before we send data we will send data length to Slave*/
-		uint8_t data_len = strlen(data);
-		SPI_SendData(SPI2, &data_len, 1);
+		//lets confirm SPI is not busy
+		while( SPI_GetFlagStatus(SPI2,SR_BUSY_FLAG) );
+//		GPIO_WriteToOutputPin(GPIOA, GPIO_PIN_NO_5, 1);
+		//Disable the SPI2 peripheral
+		SPI_EnableOrDisable(SPI2,DISABLE);;
+	}
 
-		/*Send data*/
-		SPI_SendData(SPI2,(uint8_t*)data,data_len);
+	return 0;
 
-		/*Before we disable SPI we need to confirm SPI is not busy and we can do that by checking the BSY flag of SR register */
-		while(SPI_GetFlagStatus(SPI2, SR_BUSY_FLAG));//IF BSY bit is 1 SPI is Busy else free and we can Deactivate.
-
-		/*Disable SPI*/
-		SPI_EnableOrDisable(SPI2,DISABLE);
-    }
-    return 0;
 }
