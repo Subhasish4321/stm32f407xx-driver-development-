a125d7bf677a2d149913e5e0c73ebcf01fb27afc
Subhasish Singha <125857603+Subhasish4321@users.noreply.github.com>
2026-06-17 15:59:54 +0530

    Update stm32f407xx_spi_driver.c

--- stm32f4xx_drivers/drivers/src/stm32f407xx_spi_driver.c
+++ stm32f4xx_drivers/drivers/src/stm32f407xx_spi_driver.c
@@ -291,11 +291,50 @@ void SPI_ReceiveData(SPI_RegDef_t *pSPIx,uint8_t *pRxBuffer,uint32_t len)
  */
 void SPI_IRQ_IT_Config(uint8_t IRQNumber,uint8_t EnorDis)
 {
+	if(EnorDis == ENABLE)
+	{
+		if(IRQNumber <= 31 )
+		{
+			//Program NVIC_ISER0 according to the cortex-M4 user guide.
+			*NVIC_ISER0 |= (1 << IRQNumber); // Dereference the address and set the value
+		}else if(IRQNumber >31 && IRQNumber < 64){
+			//Program ISER1
+			*NVIC_ISER1 |= ( 1 << (IRQNumber % 32));
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96)
+		{
+			//program ISER2
+			*NVIC_ISER2 |= (1 << (IRQNumber % 64 ));
+		}
+	}
+	else
+	{
+		// Program ICER (Interrupt Clear Enable Register)
+		if(IRQNumber <= 31 )
+		{
+			//Program NVIC_ICER0 according to the cortex-M4 user guide.
+			*NVIC_ICER0 |= (1 << IRQNumber); // Dereference the address and set the value
+		}else if(IRQNumber >31 && IRQNumber < 64){
+			//Program ICER1
+			*NVIC_ICER1 |= ( 1 << (IRQNumber % 32));
+		}
+		else if(IRQNumber >= 64 && IRQNumber < 96)
+		{
+			//program ICER2
+			*NVIC_ICER2 |= (1 << (IRQNumber % 64 ));
+		}
+	}
 
 }
 void SPI_IRQPriorityConfig(uint8_t IRQNumber,uint32_t IRQPriority)
 {
-
+	//1.IPR register
+	uint8_t iprx = IRQNumber/4;
+	//2.Find the section to write data to
+	uint8_t iprx_section = IRQNumber % 4;
+	//3.We are Doing iprx_section*8 because Section 1 means we start from 8th bit and not 1st bit.
+	uint8_t shift_amount = (iprx_section*8) + (8 - NO_OF_PR_BITS_IMPLEMENTED);
+	*(NVIC_PR_BASEADDR + iprx) |= (IRQPriority  << ( shift_amount ) );
 }
 void SPI_IRQHandling(SPI_Handle_t *pHandle)
 {
