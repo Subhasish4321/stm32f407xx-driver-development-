0286adcb0fdda5964e4d31d30943209e8476b963
Singha <A0095101@kiongroup.com>
2026-06-25 19:34:24 +0530

    added stm32f407xx_i2c_driver files

---
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h
@@ -0,0 +1,14 @@
+/**
+ * @file stm32f407xx_i2c_driver.h
+ * @brief Header file for I2C driver for STM32F407xx microcontroller.
+ * @author Subhasish Singha
+ * @date 2024-06-15
+ */
+
+ /**
+  * Configuration structure for I2C peripheral.
+  */
+ typedef struct
+ {
+     
+ } I2C_Config_t;
